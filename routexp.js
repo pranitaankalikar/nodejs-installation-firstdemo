f572caf1b90284c43cc528b1b457b7f74a477de0
pranitaankalikar <82073936+pranitaankalikar@users.noreply.github.com>
2021-05-28 22:02:05 +0530

    Debugging Techniques

--- routexp.js
+++ routexp.js
@@ -16,9 +16,12 @@ const requestHandler = (req, res) => {
     const body = [];
     req.on('data', chunk => {
       console.log(chunk);
+
       body.push(chunk);
     });
     return req.on('end', () => {
+      
+
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
       fs.writeFile('message.txt', message, err => {
