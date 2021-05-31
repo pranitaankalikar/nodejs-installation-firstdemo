18e0249dcd3ee8a27c68a34d6e094db9ea56ea3e
pranitaankalikar <pranitaankalikar@gmail.com>
2021-05-31 14:10:07 +0530

    test

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,7 @@ router.get('/add-product', (req, res, next) => {
 
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
-  res.redirect('/Hello');
+  res.redirect('/');
 });
 
 module.exports = router;
