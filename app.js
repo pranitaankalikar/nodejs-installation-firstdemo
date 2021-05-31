2e21e383f77f492d89d7ada6b1c9256bff8c1498
pranitaankalikar <pranitaankalikar@gmail.com>
2021-05-31 14:07:43 +0530

    route change

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const bodyParser=require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/addproduct',function(req, res, next){
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
+
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
-app.use('/product',function(req, res, next){
-    console.log(req.body);
-    res.redirect('/')
-});
-  
-app.use('/',function(req, res, next){
-  res.send('<h1>Hello from Express!</h1>');
-});
+
 app.listen(5000);
